6d0a8c60b87082eee4b9b62068f1203c524e8788
Shadman <shadmanchowdhury87@gmail.com>
2020-07-28 13:51:09 +0600

    Theme Switcher Added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,34 @@
-console.log("It's working")
+let theme = localStorage.getItem('theme')
+
+if(theme == null) {
+	setTheme('light')
+} else {
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for(var i = 0; i < themeDots.length; i++) {
+	themeDots[i].addEventListener('click', function function_name(argument) {
+		let mode = this.dataset.mode
+		console.log('Clicked', mode)
+		setTheme(mode)
+	})	
+}
+
+function setTheme(mode) {
+	if(mode == 'light') {
+		document.getElementById('theme-style').href = "assets/css/style.css"
+	}
+	if(mode == 'blue') {
+		document.getElementById('theme-style').href = "assets/css/blue.css"
+	}
+	if(mode == 'green') {
+		document.getElementById('theme-style').href = "assets/css/green.css"
+	}
+	if(mode == 'purple') {
+		document.getElementById('theme-style').href = "assets/css/purple.css"
+	}
+
+	localStorage.setItem('theme', mode)
+}
